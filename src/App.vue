cdd45b247d29e5a64113cf62d13d4d7867fc55a3
ouuyu <yuuuu@outlook.in>
2025-08-03 23:02:40 +0800

    feat(App): add router readiness check and conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ const route = useRoute()
 const router = useRouter()
 const isCollapse = ref(false)
 const isMobileScreen = ref(false)
+const isRouterReady = ref(false)
 
 const isCountdownPage = computed(() => route.path === '/shutdown-confirm')
 
@@ -55,6 +56,9 @@ function checkScreenSize() {
 onMounted(() => {
   checkScreenSize()
   window.addEventListener('resize', checkScreenSize)
+  router.isReady().then(() => {
+    isRouterReady.value = true
+  })
 })
 
 onUnmounted(() => {
@@ -63,64 +67,66 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div v-if="isCountdownPage" class="h-full w-full">
-    <ShutdownConfirm />
-  </div>
+  <div v-if="isRouterReady" class="h-full w-full">
+    <div v-if="isCountdownPage" class="h-full w-full">
+      <ShutdownConfirm />
+    </div>
 
-  <div v-else class="h-full w-full">
-    <el-container class="min-h-screen">
-      <el-aside
-        class="fixed left-0 top-0 h-screen shadow-md transition-all duration-300"
-        :width="isCollapse ? '64px' : '240px'"
-      >
-        <div
-          class="sidebar-header flex items-center p-4"
-          :class="isCollapse ? 'justify-center' : 'justify-between'"
+    <div v-if="!isCountdownPage" class="h-full w-full">
+      <el-container class="min-h-screen">
+        <el-aside
+          class="fixed left-0 top-0 h-screen shadow-md transition-all duration-300"
+          :width="isCollapse ? '64px' : '240px'"
         >
-          <el-image
-            v-show="!isCollapse"
-            src="/vite.svg"
-            class="h-8 w-8 transition-opacity"
-            alt="Logo"
-          />
-          <el-tooltip :content="isCollapse ? '展开菜单' : '收起菜单'" placement="right">
-            <el-button
-              :icon="isCollapse ? 'Expand' : 'Fold'"
-              circle
-              size="small"
-              @click="toggleCollapse"
+          <div
+            class="sidebar-header flex items-center p-4"
+            :class="isCollapse ? 'justify-center' : 'justify-between'"
+          >
+            <el-image
+              v-show="!isCollapse"
+              src="/vite.svg"
+              class="h-8 w-8 transition-opacity"
+              alt="Logo"
             />
-          </el-tooltip>
-        </div>
+            <el-tooltip :content="isCollapse ? '展开菜单' : '收起菜单'" placement="right">
+              <el-button
+                :icon="isCollapse ? 'Expand' : 'Fold'"
+                circle
+                size="small"
+                @click="toggleCollapse"
+              />
+            </el-tooltip>
+          </div>
 
-        <el-menu
-          :default-active="activeIndex"
-          class="border-0"
-          :collapse="isCollapse"
-          :collapse-transition="false"
-          @select="handleSelect"
-        >
-          <el-menu-item v-for="item in menuItems" :key="item.path" :index="item.path">
-            <el-icon>
-              <component :is="item.icon" />
-            </el-icon>
-            <template #title>
-              {{ item.title }}
-            </template>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
+          <el-menu
+            :default-active="activeIndex"
+            class="border-0"
+            :collapse="isCollapse"
+            :collapse-transition="false"
+            @select="handleSelect"
+          >
+            <el-menu-item v-for="item in menuItems" :key="item.path" :index="item.path">
+              <el-icon>
+                <component :is="item.icon" />
+              </el-icon>
+              <template #title>
+                {{ item.title }}
+              </template>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
 
-      <el-container :style="{ marginLeft: isCollapse ? '64px' : '240px' }">
-        <el-main class="p-4">
-          <router-view v-slot="{ Component }">
-            <transition name="fade" mode="out-in">
-              <component :is="Component" />
-            </transition>
-          </router-view>
-        </el-main>
+        <el-container :style="{ marginLeft: isCollapse ? '64px' : '240px' }">
+          <el-main class="p-4">
+            <router-view v-slot="{ Component }">
+              <transition name="fade" mode="out-in">
+                <component :is="Component" />
+              </transition>
+            </router-view>
+          </el-main>
+        </el-container>
       </el-container>
-    </el-container>
+    </div>
   </div>
 </template>
 
